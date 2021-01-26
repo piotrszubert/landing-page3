f398ac7a8be63f1f4d5677f0532bbe68ee77c18f
Piotr Szubert <piotr.szubert777@gmail.com>
2021-01-26 19:21:52 +0100

    more fixes..

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const openNav = () => {
 const closeNav = () => {
     myNav.style.height = "0%";
 }  
+
+
+
+//animate numbers 
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj = document.getElementById("value");
+  animateValue(obj, 0, 100, 1000);
